79ef8b6e115c95e7d03c33eb66913e8bd582a0a0
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-06-06 16:05:00 -0500

    remove unnecessary function and add comments

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 
 // get Subtitle Heading
 var menuSubtitle = document.querySelector('.styles-menu__subtitle');
+// get menu items
+var menuItems = document.querySelectorAll('.styles-menu__item');
+// toggle hidden on menu items
 menuSubtitle.addEventListener('click', hide);
+// toggle style change on click for menu items
+menuItems.forEach(item => item.addEventListener('click', makeSubtitle));
 
 function hide() {
-  var menuItems = document.querySelectorAll('.styles-menu__item');
   menuItems.forEach(item => item.classList.toggle('hidden'));
 }
 
-function makeSubtitle(){
-  this.className = menuSubtitle.className;
-}
