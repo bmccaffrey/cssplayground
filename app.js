82fb011e537b4715cc7e62e313bfcdc165c00f49
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-06-06 17:54:48 -0500

    allow toggling of properties

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-'use strict';
+"use strict";
 
-// get Subtitle Heading
-var menuSubtitle = document.querySelector('.styles-menu__subtitle');
-// get menu items
+var subtitle = document.querySelector('.styles-menu__subtitle');
+
 var menuItems = document.querySelectorAll('.styles-menu__item');
-// toggle hidden on menu items
-menuSubtitle.addEventListener('click', hide);
-// toggle style change on click for menu items
-menuItems.forEach(item => item.addEventListener('click', makeSubtitle));
 
-function hide() {
-  menuItems.forEach(item => item.classList.toggle('hidden'));
+var flex = menuItems[0];
+var flexProperties = flex.nextElementSibling;
+
+var grid = menuItems[1];;
+var gridProperties = grid.nextElementSibling;
+
+subtitle.addEventListener('click', hideNotSubtitle);
+
+function hideNotSubtitle() {
+  flex.classList.toggle('hidden');
+  grid.classList.toggle('hidden');
 }
 
+flex.addEventListener('click', hideNotFlex);
+
+function hideNotFlex() {
+  subtitle.classList.toggle('hidden');
+  grid.classList.toggle('hidden');
+}
+
+grid.addEventListener('click', hideNotGrid);
+
+function hideNotGrid() {
+  subtitle.classList.toggle('hidden');
+  flex.classList.toggle('hidden');
+}
+
+flex.addEventListener('click', toggleProperties);
+grid.addEventListener('click', toggleProperties);
+
+function toggleProperties() {
+  this.nextElementSibling.classList.toggle('hidden');
+}
